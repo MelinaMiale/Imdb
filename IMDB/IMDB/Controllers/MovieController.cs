39c3a151f3fa75b86e0b462901a16393a14a9891
Melina Miale <mmiale@quadiontech.com>
2020-02-12 18:13:21 -0300

    #22 se modifica controlador para que envie modelo de tipo MovieViewModel, tambien se modifica la vista para que tome los cambios en la accion que lista las peliculas

--- IMDB/IMDB/Controllers/MovieController.cs
+++ IMDB/IMDB/Controllers/MovieController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IMDB.Models;
 using Microsoft.AspNetCore.Mvc;
+using Proyect_Models;
 using Repository;
 
 namespace IMDB.Controllers
@@ -16,10 +18,42 @@ namespace IMDB.Controllers
             db = new InMemoryMovieStorage();
         }
 
+        //metodo que copia la informacion de un objeto de tipo Movie a un objeto de tipo MovieViewModel, y devuelve este ultimo
+        private MovieViewModel MapMovietoMovieViewModel (Movie movie, MovieViewModel movieViewModel)
+        {
+            movieViewModel.Id = movie.ID_movie;
+            movieViewModel.Name = movie.Name;
+            movieViewModel.FlyerUrl = movie.FlyerUrl;
+
+            return movieViewModel;
+        }
+
+
         public ViewResult Index()
         {
-            var model = db.GetAll();// creo un modelo donde voy a ingresar todas las peliculas, var en este caso toma la forma de List<Proyect_Model.Movies>
-            return View(model); //debo enviar el modelo por parametro para que la vista obtenga la informacion
+            //obtengo lita de peliculas guardadas en storage
+            var movies = db.GetAll();
+
+            //crear una lista en base al modelo MovieViewModel
+            var movieViewModel = new List<MovieViewModel>();
+
+            //implemento metodo para cada pelicula en la lista: movies, y las agrego a la lista: movieViewModel
+            foreach(var movie in movies)
+            {
+                movieViewModel.Add(MapMovietoMovieViewModel(movie, new MovieViewModel()));
+            }
+            /*
+             * otra forma de hacerlo es usando LinQ (similar al stream en Java)
+             
+                      var movieViewModels = movies.Select(m => new MovieViewModel {
+                           Id = m.ID_movie,
+                           FlyerUrl = m.FlyerUrl,
+                           Name = m.Name
+                      }).ToList(); 
+             
+             */
+
+            return View(movieViewModel);// envio la lista de tipo MovieViewModel que contiene las peliculas con ese modelo 
         }
 
         /*
