aeb9a6bb442470b2fb2a74d135b4512cadab1eaf
Melina Miale <mmiale@quadiontech.com>
2020-02-13 14:07:00 -0300

    #20 se agrega accion en controlador y modeloDeVista para mostrar los detalles de una pelicula. tambien se agrega a la interfaz el metodo para buscar por id

---
+++ IMDB/IMDB/Models/MovieViewDetails.cs
@@ -0,0 +1,49 @@
+﻿using Proyect_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMDB.Models
+{
+    public class MovieViewDetails
+    {
+		public long ID
+		{
+			get;
+			set;
+		}
+
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public Nationality Nationality
+		{
+			get;
+			set;
+		}
+
+		public List<Rol> Cast
+		{
+			get;
+			set;
+		}
+
+		public DateTime ReleaseDate
+		{
+			get;
+			set;
+		}
+
+		public string FlyerUrl
+		{
+			get;
+			set;
+		}
+
+
+	}
+}
--- IMDB/IMDB/Storage/InMemoryMovieStorage.cs
+++ IMDB/IMDB/Storage/InMemoryMovieStorage.cs
@@ -8,98 +8,77 @@ namespace Repository
     public class InMemoryMovieStorage : IRepository
 
     {
-       public InMemoryMovieStorage(){}
+        public InMemoryMovieStorage() { }
 
-           public List<Movie> GetAll()
-           {
-               
-               //Lista de roles
-               Rol rol5 = new Rol(5, "Emil Rottmayer");
-               Rol rol6 = new Rol(6, "Ray Breslin");
-               Rol rol7 = new Rol(7, "Hobbes");
-               Rol rol8 = new Rol(8, "Dr. Kyrie");
-               List<Rol> CastMovie1 = new List<Rol>();
+        public List<Movie> GetAll()
+        {
 
-               Rol rol1 = new Rol(1, "BridgetJones");
-               Rol rol2 = new Rol(2, "Mark Darcy");
-               Rol rol3 = new Rol(3, "Daniel Cleaver");
-               Rol rol4 = new Rol(4, "Bridget's Mum");
-               List<Rol> CastMovie2 = new List<Rol>();
+            //Pelicula 1
+            Rol rol5 = new Rol(5, "Emil Rottmayer");
+            Rol rol6 = new Rol(6, "Ray Breslin");
+            Rol rol7 = new Rol(7, "Hobbes");
+            Rol rol8 = new Rol(8, "Dr. Kyrie");
+            List<Rol> CastMovie2 = new List<Rol>();
+            CastMovie2.Add(rol5); CastMovie2.Add(rol6); CastMovie2.Add(rol7); CastMovie2.Add(rol8);
+            Movie movie2 = new Movie(2, "Escape Plan");
+            DateTime Movie2ReleaseDate = new DateTime(2013, 10, 31);
+            movie2.ReleaseDate = Movie2ReleaseDate;
+            movie2.FlyerUrl = @"img\bridgetJonesPoster.jpg";
+            movie2.Nationality = Nationality.american;
+            movie2.Cast = CastMovie2;
 
-               CastMovie1.Add(rol1);
-               CastMovie1.Add(rol2);
-               CastMovie1.Add(rol3);
-               CastMovie1.Add(rol4);
-               CastMovie2.Add(rol5);
-               CastMovie2.Add(rol6);
-               CastMovie2.Add(rol7);
-               CastMovie2.Add(rol8);
-              
+            //Pelicula 2
+            Rol rol1 = new Rol(1, "BridgetJones");
+            Rol rol2 = new Rol(2, "Mark Darcy");
+            Rol rol3 = new Rol(3, "Daniel Cleaver");
+            Rol rol4 = new Rol(4, "Bridget's Mum");
+            List<Rol> CastMovie1 = new List<Rol>();
+            CastMovie1.Add(rol1); CastMovie1.Add(rol2); CastMovie1.Add(rol3); CastMovie1.Add(rol4);
+            Movie movie1 = new Movie(1, "Bridget Jones Diary");
+            DateTime Movie1ReleaseDate = new DateTime(2001, 4, 4);
+            movie1.ReleaseDate = Movie1ReleaseDate;
+            movie1.FlyerUrl = @"\img\EscapePlanPoster.jpg";
+            movie1.Nationality = Nationality.american;
+            movie1.Cast = CastMovie1;
 
-               //peliculas
-               Movie movie1 = new Movie(1, "BridgetJones Diary");
-               DateTime Movie1ReleaseDate = new DateTime(2001, 4, 4);
-               movie1.ReleaseDate = Movie1ReleaseDate;
-               movie1.FlyerUrl = "http://www.impawards.com/2001/posters/bridget_joness_diary_ver1.jpg";
-               movie1.Nationality = Nationality.american;
-               movie1.Cast = CastMovie1;
+            //Pelicula 3
+            Rol rol9 = new Rol(5, "Sally");
+            Rol rol10 = new Rol(6, "Harry");
+            Rol rol11 = new Rol(7, "Marie");
+            Rol rol12 = new Rol(8, "Jess");
+            List<Rol> CastMovie3 = new List<Rol>();
+            CastMovie3.Add(rol9); CastMovie3.Add(rol10); CastMovie3.Add(rol11); CastMovie3.Add(rol12);
+            Movie movie3 = new Movie(2, "When Harry met Sally");
+            DateTime Movie3ReleaseDate = new DateTime(1989, 07, 12);
+            movie3.ReleaseDate = Movie3ReleaseDate;
+            movie3.FlyerUrl = @"img\HarrySallyPoster.jpg";
+            movie3.Nationality = Nationality.american;
+            movie3.Cast = CastMovie3;
 
-               Movie movie2 = new Movie(2, "Escape Plan");
-               DateTime Movie2ReleaseDate = new DateTime(2013, 10, 31);
-               movie2.ReleaseDate = Movie2ReleaseDate;
-               movie2.FlyerUrl = "https://images-na.ssl-images-amazon.com/images/I/71DKbhlrU1L._AC_SL1008_.jpg";
-               movie2.Nationality = Nationality.american;
-               movie2.Cast = CastMovie2;
-
-            return new List<Movie> { movie1, movie2 };
-           }
+            return new List<Movie> { movie1, movie2, movie3 };
+        }
 
         public Movie GetById(long Id)
         {
-            throw new NotImplementedException();
+            List<Movie> movies = GetAll();
+            var movieById = new Movie();
+
+            foreach (var movie in movies)
+            {
+                if(movie.ID_movie == Id)
+                {
+                    movieById = movie;
+                }
+                
+            }
+
+            return movieById;
         }
 
-        
+
 
     }
 
 
 }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-       
 
-        
