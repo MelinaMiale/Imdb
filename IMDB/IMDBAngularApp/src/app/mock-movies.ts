75059d41663f2059fafde9cf8358c7098f91b38b
Melina Miale <mmiale@quadiontech.com>
2020-03-17 19:58:58 -0300

    #62 se trabaja en la pagina de inicio y listado de peliculas. issue 62 wip

---
+++ IMDB/IMDBAngularApp/src/app/mock-movies.ts
@@ -0,0 +1,7 @@
+import { Movie } from './classes/movie';
+
+export const MOVIES : Movie [] = [
+  { id: 1, name: 'Mortal Kombat'},
+  { id: 2, name: 'Escape Plan'},
+  { id: 3, name: 'Bridget Jones Diary'}
+];
